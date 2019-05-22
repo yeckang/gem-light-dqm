a3412961d9d25001373fc3e62e8d5ea1e4698569
giovanni-mocellin <gio.moc@gmail.com>
2019-05-22 17:15:40 +0200

    Conforming to the indentation style of the code package: 1tab=2spaces

--- dqm-root/src/common/AMC_histogram.cxx
+++ dqm-root/src/common/AMC_histogram.cxx
@@ -4,97 +4,100 @@
 //!A class creating histograms for AMC data
 class AMC_histogram: public Hardware_histogram
 {
-  public:
-    //!Constructor calls the base constructor of Hardware_histogram. Requires a string for filename, directory, and another string.
-    AMC_histogram(const std::string & filename, TDirectory * dir, const std::string & hwid):Hardware_histogram(filename, dir, hwid){}//call base constructor
-    ~AMC_histogram()
-    {
-        delete[] m_gebsH;
+public:
+  //!Constructor calls the base constructor of Hardware_histogram. Requires a string for filename, directory, and another string.
+  AMC_histogram(const std::string & filename, TDirectory * dir, const std::string & hwid):Hardware_histogram(filename, dir, hwid){}//call base constructor
+  ~AMC_histogram()
+  {
+    delete[] m_gebsH;
+  }
+  
+  //!Books histograms for AMC data
+  /*!
+   This books histograms for the following data: AMC number, L1A number, Bunch Crossing ID, Data Length, Format Version, Run Type, Param 1, 2 and 3, Orbit Number, Board ID, GEM DAV list, Buffer Status, GEM DAV count, TTS state, Chamber Timeout, OOS GLIB sync, CRC, L1AT, and DlengthT
+   */
+  void bookHistograms()
+  {
+    m_gebsH = new GEB_histogram*[12];
+    for (unsigned int i = 0; i<12; i++){
+      m_gebsH[i] = 0;
     }
-
-    //!Books histograms for AMC data
-    /*!
-    This books histograms for the following data: AMC number, L1A number, Bunch Crossing ID, Data Length, Format Version, Run Type, Param 1, 2 and 3, Orbit Number, Board ID, GEM DAV list, Buffer Status, GEM DAV count, TTS state, Chamber Timeout, OOS GLIB sync, CRC, L1AT, and DlengthT
-    */
-    void bookHistograms()
-    {
-      m_gebsH = new GEB_histogram*[12];
-      for (unsigned int i = 0; i<12; i++){
-          m_gebsH[i] = 0;
-      }
-      m_dir->cd();
-      AMCnum     	 = new TH1F("AMCnum", "AMC number", 12,  0, 12);
-      //L1A        = new TH1F("L1A", "L1A ID", 0xffffff,  0x0, 0xffffff);
-      L1A_mismatch = new TH1F("L1A_mismatch", "L1A_mismatch (AMC L1A - AMC13 LV1_idT)", 40,  -20.0, 20.0);
-      BX           = new TH1F("BX", "BX ID", 4095,  0x0, 0xfff);
-      //Dlength    = new TH1F("Dlength", "Data Length", 0xfffff,  0x0, 0xfffff);
-      FV         	 = new TH1F("FV", "Format Version", 15,  0x0, 0xf);
-      Rtype      	 = new TH1F("Rtype", "Run Type", 15,  0x0, 0xf);
-      Param1     	 = new TH1F("Param1", "Run Param 1", 256,  -0.5, 255.5);
-      Param2     	 = new TH1F("Param2", "Run Param 2", 256,  -0.5, 255.5);
-      Param3     	 = new TH1F("Param3", "Run Param 3", 256,  -0.5, 255.5);
-      Onum       	 = new TH1F("Onum", "Orbit Number", 0xffff,  0, 0xffff);
-      BID        	 = new TH1F("BID", "Board ID", 0xffff,  0, 0xffff);
-      GEMDAV     	 = new TH1F("GEMDAV", "GEM DAV list", 24,  0, 24);
-      Bstatus    	 = new TH1F("Bstatus", "Buffer Status", 24,  0, 24);
-      GDcount    	 = new TH1F("GDcount", "GEM DAV count", 32,  0, 32);
-      Tstate     	 = new TH1F("Tstate", "TTS state", 15,  0, 15);
-      ChamT      	 = new TH1F("ChamT", "Chamber Timeout", 24, 0, 24);
-      OOSG       	 = new TH1F("OOSG", "OOS GLIB", 1, 0, 1);
+    m_dir->cd();
+    AMCnum       = new TH1F("AMCnum", "AMC number", 12,  0, 12);
+    //L1A        = new TH1F("L1A", "L1A ID", 0xffffff,  0x0, 0xffffff);
+    L1A_mismatch = new TH1F("L1A_mismatch", "L1A_mismatch (AMC L1A - AMC13 LV1_idT)", 40,  -20.0, 20.0);
+    BX           = new TH1F("BX", "BX ID", 4095,  0x0, 0xfff);
+    //Dlength    = new TH1F("Dlength", "Data Length", 0xfffff,  0x0, 0xfffff);
+    FV           = new TH1F("FV", "Format Version", 15,  0x0, 0xf);
+    Rtype        = new TH1F("Rtype", "Run Type", 15,  0x0, 0xf);
+    Param1       = new TH1F("Param1", "Run Param 1", 256,  -0.5, 255.5);
+    Param2       = new TH1F("Param2", "Run Param 2", 256,  -0.5, 255.5);
+    Param3       = new TH1F("Param3", "Run Param 3", 256,  -0.5, 255.5);
+    Onum         = new TH1F("Onum", "Orbit Number", 0xffff,  0, 0xffff);
+    BID          = new TH1F("BID", "Board ID", 0xffff,  0, 0xffff);
+    GEMDAV       = new TH1F("GEMDAV", "GEM DAV list", 24,  0, 24);
+    Bstatus      = new TH1F("Bstatus", "Buffer Status", 24,  0, 24);
+    GDcount      = new TH1F("GDcount", "GEM DAV count", 32,  0, 32);
+    Tstate       = new TH1F("Tstate", "TTS state", 15,  0, 15);
+    ChamT        = new TH1F("ChamT", "Chamber Timeout", 24, 0, 24);
+    OOSG         = new TH1F("OOSG", "OOS GLIB", 1, 0, 1);
+  }
+  
+  //!Fills the histograms for AMC data
+  /*!
+   This fills the histograms for the following data: AMC number, L1A number, Bunch Crossing ID, Data Length, Format Version, Run Type, Param 1, 2 and 3, Orbit Number, Board ID, GEM DAV list, Buffer Status, GEM DAV count, TTS state, Chamber Timeout, OOS GLIB sync, CRC, L1AT, and DlengthT
+   */
+  void fillHistograms(AMCdata *amc, AMC13Event *amc13){
+    AMCnum->Fill(amc->AMCnum());
+    //L1A->Fill(amc->L1A());
+    L1A_mismatch->Fill(amc->L1A()-amc13->LV1_idT());
+    BX->Fill(amc->BX());
+    //Dlength->Fill(amc->Dlength());
+    FV->Fill(amc->FV());
+    Rtype->Fill(amc->Rtype());
+    Param1->Fill(amc->Param1());
+    Param2->Fill(amc->Param2());
+    Param3->Fill(amc->Param3());
+    Onum->Fill(amc->Onum());
+    BID->Fill(amc->BID());
+    GDcount->Fill(amc->GDcount());
+    Tstate->Fill(amc->Tstate());
+    OOSG->Fill(amc->OOSG());
+    uint8_t binFired = 0;
+    for (int bin = 0; bin < 24; bin++){
+      binFired = ((amc->GEMDAV() >> bin) & 0x1);
+      if (binFired) GEMDAV->Fill(bin);
+      binFired = ((amc->Bstatus() >> bin) & 0x1);
+      if (binFired) Bstatus->Fill(bin);
+      binFired = ((amc->ChamT() >> bin) & 0x1);
+      if (binFired) ChamT->Fill(bin);
     }
-
-     //!Fills the histograms for AMC data
-    /*!
-    This fills the histograms for the following data: AMC number, L1A number, Bunch Crossing ID, Data Length, Format Version, Run Type, Param 1, 2 and 3, Orbit Number, Board ID, GEM DAV list, Buffer Status, GEM DAV count, TTS state, Chamber Timeout, OOS GLIB sync, CRC, L1AT, and DlengthT
-    */
-    void fillHistograms(AMCdata *amc, AMC13Event *amc13){
-      AMCnum->Fill(amc->AMCnum());
-      //L1A->Fill(amc->L1A());
-      L1A_mismatch->Fill(amc->L1A()-amc13->LV1_idT());
-      BX->Fill(amc->BX());
-      //Dlength->Fill(amc->Dlength());
-      FV->Fill(amc->FV());
-      Rtype->Fill(amc->Rtype());
-      Param1->Fill(amc->Param1());
-      Param2->Fill(amc->Param2());
-      Param3->Fill(amc->Param3());
-      Onum->Fill(amc->Onum());
-      BID->Fill(amc->BID());
-      GDcount->Fill(amc->GDcount());
-      Tstate->Fill(amc->Tstate());
-      OOSG->Fill(amc->OOSG());
-      uint8_t binFired = 0;
-      for (int bin = 0; bin < 24; bin++){
-        binFired = ((amc->GEMDAV() >> bin) & 0x1);
-        if (binFired) GEMDAV->Fill(bin);
-        binFired = ((amc->Bstatus() >> bin) & 0x1);
-        if (binFired) Bstatus->Fill(bin);
-        binFired = ((amc->ChamT() >> bin) & 0x1);
-        if (binFired) ChamT->Fill(bin);
-      }
-    }
-    //!Adds a GEB_histogram object to m_gebsH vector
-    void addGEBH(GEB_histogram* gebH, int i){m_gebsH[i]=gebH;}
-    //!Returns the m_gebsH vector
-    GEB_histogram* gebsH(int i){return m_gebsH[i];}
-  private:
-    GEB_histogram **m_gebsH;   			///<A vector of GEB_histogram
-    TH1F* AMCnum;                         ///<Histogram for AMC number
-    TH1F* L1A;                            ///<Histogram for L1A number
-    TH1F* L1A_mismatch;										///<Histogram for L1A vs LV1_idT mismatch
-    TH1F* BX;                             ///<Histogram for Bunch Crossing ID
-    TH1F* Dlength;                        ///<Histogram for Data Length
-    TH1F* FV;                             ///<Histogram for Format Version
-    TH1F* Rtype;                          ///<Histogram for Run Type
-    TH1F* Param1;                         ///<Histogram for Parameter 1
-    TH1F* Param2;                         ///<Histogram for Parameter 2
-    TH1F* Param3;                         ///<Histogram for Parameter 3
-    TH1F* Onum;                           ///<Histogram for Orbit Number
-    TH1F* BID;                            ///<Histogram for Board ID
-    TH1F* GEMDAV;                         ///<Histogram for GEM DAV list
-    TH1F* Bstatus;                        ///<Histogram for Buffer Status
-    TH1F* GDcount;                        ///<Histogram for GEM DAV count
-    TH1F* Tstate;                         ///<Histogram for TTS state
-    TH1F* ChamT;                          ///<Histogram for Chamber Timeout
-    TH1F* OOSG;                           ///<Histogram for OOS GLIB
+  }
+  //!Adds a GEB_histogram object to m_gebsH vector
+  void addGEBH(GEB_histogram* gebH, int i){m_gebsH[i]=gebH;}
+  //!Returns the m_gebsH vector
+  GEB_histogram* gebsH(int i){return m_gebsH[i];}
+private:
+  GEB_histogram **m_gebsH;              ///<A vector of GEB_histogram
+  TH1F* AMCnum;                         ///<Histogram for AMC number
+  TH1F* L1A;                            ///<Histogram for L1A number
+  TH1F* L1A_mismatch;                   ///<Histogram for L1A vs LV1_idT mismatch
+  TH1F* BX;                             ///<Histogram for Bunch Crossing ID
+  TH1F* Dlength;                        ///<Histogram for Data Length
+  TH1F* FV;                             ///<Histogram for Format Version
+  TH1F* Rtype;                          ///<Histogram for Run Type
+  TH1F* Param1;                         ///<Histogram for Parameter 1
+  TH1F* Param2;                         ///<Histogram for Parameter 2
+  TH1F* Param3;                         ///<Histogram for Parameter 3
+  TH1F* Onum;                           ///<Histogram for Orbit Number
+  TH1F* BID;                            ///<Histogram for Board ID
+  TH1F* GEMDAV;                         ///<Histogram for GEM DAV list
+  TH1F* Bstatus;                        ///<Histogram for Buffer Status
+  TH1F* GDcount;                        ///<Histogram for GEM DAV count
+  TH1F* Tstate;                         ///<Histogram for TTS state
+  TH1F* ChamT;                          ///<Histogram for Chamber Timeout
+  TH1F* OOSG;                           ///<Histogram for OOS GLIB
 };
+
+
+
--- dqm-root/src/common/VFAT_histogram.cxx
+++ dqm-root/src/common/VFAT_histogram.cxx
@@ -14,39 +14,39 @@ public:
   //!Calls the base constructor
   VFAT_histogram(VFAT_histogram * vH):Hardware_histogram("dummy", vH->m_dir, vH->m_HWID){}//call base constructor
   ~VFAT_histogram(){}
-
+  
   //!Books histograms for VFAT data
   /*!
-    This books histograms for the following data: Difference between crc and recalculated crc, Control Bit 1010, Control Bit 1100, Control Bit 1110, Bunch Crossing Number, Event Counter, Control Flags, and Chip ID, and Fired Channels. It also creates a subdirectory for Threshold Scans and books those histograms.
-  */
+   This books histograms for the following data: Difference between crc and recalculated crc, Control Bit 1010, Control Bit 1100, Control Bit 1110, Bunch Crossing Number, Event Counter, Control Flags, and Chip ID, and Fired Channels. It also creates a subdirectory for Threshold Scans and books those histograms.
+   */
   void bookHistograms(){
     m_dir->cd();
     n_hits_per_event = new TH1F("n_hits_per_event", "n_hits_per_event", 129,  -0.5 , 128.5);
-    BC       				 = new TH1F("BC", "Bunch Crossing Number", 4096,  -0.5, 4095.5);
-    BC_mismatch 		 = new TH1F("BC_mismatch", "Bunch Crossing VFAT-AMC (BX - BC ID) mismatch", 40,  -20.0, 20.0);
-    EC       				 = new TH1F("EC", "Event Counter", 255,  0x0 , 0xff);
-    EC_mismatch 		 = new TH1F("EC_mismatch", "Event Counter VFAT-AMC13 (event counter - LV1_idT) mismatch", 40,  -20.0, 20.0);
-    Header     			 = new TH1F("Header", "Header", 32,  0x0 , 0xff);
-    SlotN    				 = new TH1F("SlotN", "Slot Number", 24,  0, 24);
-    FiredChannels 	 = new TH1F("FiredChannels", "FiredChannels", 128, -0.5, 127.5);
-    latencyScan   	 = new TH1F("latencyScan",   "Latency Scan", 1024,  -0.5, 1023.5);
-    latencyScan2D 	 = new TH2F("latencyScan2D", "Latency Scan: Chan Vs Latency", 1024, -0.5, 1023.5, NCHANNELS, -0.5, NCHANNELS-0.5);
+    BC               = new TH1F("BC", "Bunch Crossing Number", 4096,  -0.5, 4095.5);
+    BC_mismatch      = new TH1F("BC_mismatch", "Bunch Crossing VFAT-AMC (BX - BC ID) mismatch", 40,  -20.0, 20.0);
+    EC               = new TH1F("EC", "Event Counter", 255,  0x0 , 0xff);
+    EC_mismatch      = new TH1F("EC_mismatch", "Event Counter VFAT-AMC13 (event counter - LV1_idT) mismatch", 40,  -20.0, 20.0);
+    Header           = new TH1F("Header", "Header", 32,  0x0 , 0xff);
+    SlotN            = new TH1F("SlotN", "Slot Number", 24,  0, 24);
+    FiredChannels    = new TH1F("FiredChannels", "FiredChannels", 128, -0.5, 127.5);
+    latencyScan      = new TH1F("latencyScan",   "Latency Scan", 1024,  -0.5, 1023.5);
+    latencyScan2D    = new TH2F("latencyScan2D", "Latency Scan: Chan Vs Latency", 1024, -0.5, 1023.5, NCHANNELS, -0.5, NCHANNELS-0.5);
     const char *warning_labels[3] = {"Flag raised", "No channels fired", "Excessive channels fired"};
     const char *error_labels[1] = {"CRC mismatch"};
-    Warnings 				 = new TH1I("Warnings", "Warnings", 3,  0, 3);
+    Warnings         = new TH1I("Warnings", "Warnings", 3,  0, 3);
     for (int i = 1; i<4; i++) Warnings->GetXaxis()->SetBinLabel(i, warning_labels[i-1]);
-    Errors   				 = new TH1I("Errors", "Critical errors", 1,  0, 1);
+    Errors           = new TH1I("Errors", "Critical errors", 1,  0, 1);
     for (int i = 1; i<2; i++) Errors->GetXaxis()->SetBinLabel(i, error_labels[i-1]);
-
+    
     //get maps
     m_sn = std::stoi(m_HWID);
     readMapFromFile(m_sn,m_strip_map);
   }
-
+  
   //!Fills histograms for VFAT data
   /*!
-    This fills histograms for the following data: Difference between crc and recalculated crc, Control Bit 1010, Control Bit 1100, Control Bit 1110, Bunch Crossing Number, Event Counter, Control Flags, and Chip ID, and Fired Channels
-  */
+   This fills histograms for the following data: Difference between crc and recalculated crc, Control Bit 1010, Control Bit 1100, Control Bit 1110, Bunch Crossing Number, Event Counter, Control Flags, and Chip ID, and Fired Channels
+   */
   void fillHistograms(VFATdata * vfat, AMCdata *amc, AMC13Event *amc13, long long int orbitNumber){
     BC->Fill(vfat->BC());
     BC_mismatch->Fill(vfat->BC() - amc->BX());
@@ -64,21 +64,21 @@ public:
     int n_hits_fired = 0;
     for (int chan = 0; chan < 128; ++chan) {
       if (chan < 64){
-				chan0xf = ((vfat->lsData() >> chan) & 0x1);
-				if(chan0xf) {
-      		n_hits_fired++;
-	  			FiredChannels->Fill(chan);
-				}
+        chan0xf = ((vfat->lsData() >> chan) & 0x1);
+        if(chan0xf) {
+          n_hits_fired++;
+          FiredChannels->Fill(chan);
+        }
       } else {
-				chan0xf = ((vfat->msData() >> (chan-64)) & 0x1);
-				if(chan0xf) {
-      		n_hits_fired++;
-	  			FiredChannels->Fill(chan);
-				}
+        chan0xf = ((vfat->msData() >> (chan-64)) & 0x1);
+        if(chan0xf) {
+          n_hits_fired++;
+          FiredChannels->Fill(chan);
+        }
       }
     }
     n_hits_per_event->Fill(n_hits_fired);
-
+    
   }
   void fillWarnings(){
     if (FiredChannels->GetEntries() == 0) {
@@ -96,36 +96,36 @@ public:
     for (int i = 0; i < 128; i++){
       uint16_t chan0xf = 0;
       if (i < 64){
-	    	chan0xf = ((vfat->lsData() >> i) & 0x1);
-	    	if(chan0xf) {
-	      	channelFired = true;
-        	latencyScan2D->Fill(latency,i);
-        	n_h_fired++;
-	    	}
+        chan0xf = ((vfat->lsData() >> i) & 0x1);
+        if(chan0xf) {
+          channelFired = true;
+          latencyScan2D->Fill(latency,i);
+          n_h_fired++;
+        }
       } else {
-	    	chan0xf = ((vfat->msData() >> (i-64)) & 0x1);
-	    	if(chan0xf) {
-	      	channelFired = true;
-        	latencyScan2D->Fill(latency,i);
-        	n_h_fired++;
-	    	}
+        chan0xf = ((vfat->msData() >> (i-64)) & 0x1);
+        if(chan0xf) {
+          channelFired = true;
+          latencyScan2D->Fill(latency,i);
+          n_h_fired++;
+        }
       }
     }// end loop on channels
     if (channelFired) {
       latencyScan->Fill(latency);
     }
   }
-
+  
   int * getMap(){ return m_strip_map;}
-
-
-
+  
+  
+  
 private:
   TH1F* n_hits_per_event;
   TH1F* BC;               ///<Histogram for Bunch Crossing Number
-  TH1F* BC_mismatch;			///<Histogram for Bunch Crossing Number mismatch between VFATs and AMC
+  TH1F* BC_mismatch;      ///<Histogram for Bunch Crossing Number mismatch between VFATs and AMC
   TH1F* EC;               ///<Histogram for Event Counter
-  TH1F* EC_mismatch;			///<Histogram for Event Counter mismatch bewteen VFATs and AMC13
+  TH1F* EC_mismatch;      ///<Histogram for Event Counter mismatch bewteen VFATs and AMC13
   TH1F* Header;           ///<Histogram for Control Flags
   TH1F* FiredChannels;    ///<Histogram for Fired Channels (uses lsData and fmData)
   TH1F* SlotN;
@@ -135,9 +135,9 @@ private:
   TH1I* Errors;
   int m_sn;
   int m_strip_map[128];
-
+  
   uint16_t vfatBlockWords[12];   ///<Array of uint16_t used for setVFATBlockWords
-
+  
   //!This puts the VFAT data in an array of uint16_t to be used for the crc check
   void setVFATBlockWords(VFATdata * vfat_)
   {
@@ -153,14 +153,14 @@ private:
     //vfatBlockWords[2]  = (0x00000000ffff0000 & vfat_->lsData()) >> 16;
     //vfatBlockWords[1]  = (0x000000000000ffff & vfat_->lsData());
   }
-
+  
   //!Recalculates the CRC to be compared to original CRC. Difference should be 0.
   uint16_t checkCRC(uint16_t dataVFAT[11])
   {
     uint16_t crc_fin = 0xffff;
     for (int i = 11; i >= 1; i--)
     {
-			crc_fin = this->crc_cal(crc_fin, dataVFAT[i]);
+      crc_fin = this->crc_cal(crc_fin, dataVFAT[i]);
     }
     return(crc_fin);
   }
@@ -172,7 +172,7 @@ private:
     bool d=0;
     uint16_t crc_temp = crc_in;
     unsigned char datalen = 16;
-
+    
     for (int i=0; i<datalen; i++){
       if (dato & v) d = 1;
       else d = 0;
@@ -183,3 +183,4 @@ private:
     return(crc_temp);
   }
 };
+
